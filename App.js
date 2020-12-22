f4e74f89a7cd4fd4bbd28b50a622f801bef3bd68
dnyeya <72346643+dnyeya@users.noreply.github.com>
2020-12-22 20:41:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+
+export class RedButton extends Component{
+  render(){
+    return(
+      <Button color = "red" title = "Click me"></Button>
+    )
+  }
+}
+export default class App extends Component {
+  render() {
+    return (
+      <View style = {{marginTop:500,width:"50%"}}>
+      <RedButton/>
+        <Text>Dnyeya</Text>
+        </View>
+    );
+  }
+}
